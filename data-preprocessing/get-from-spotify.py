69ddf8c8f659b329b313f2ef3c9813ef74b7dbfd
Matthias Heimberg <matthias.heimberg@students.ffhs.ch>
2023-10-07 17:04:33 +0200

    add data from playlists

--- data-preprocessing/get-from-spotify.py
+++ data-preprocessing/get-from-spotify.py
@@ -1,63 +1,51 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 from dotenv import load_dotenv
-load_dotenv()
 import os
 
+load_dotenv()
 
 client_id = os.getenv("SPOTIFY_CLIENT_ID")
 client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-# app credentials from spotify dashboard
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri="http://localhost:8086", scope="user-library-read"))
+# Authenticate with Spotify API
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=client_id,
+    client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# function to fetch liked songs from spotify
-def fetch_liked_songs_data():
-    """Fetch specified data for tracks in the user's Liked Songs."""
-    results = sp.current_user_saved_tracks()
-    tracks = results['items']
-    
-    song_data = []
-    while results:
-        for item in tracks:
-            track_obj = item['track']
-            track_name = track_obj['name']
-            track_id = track_obj['id']
-            album_name = track_obj['album']['name']
-            artist_name = track_obj['artists'][0]['name']
-            artist_id = track_obj['artists'][0]['id']  # Get the first artist's ID
-            
-            # Fetch artist to get the genre
-            artist = sp.artist(artist_id)
-            genres = artist['genres']
-            
-            # Fetch audio features
-            audio_features = sp.audio_features([track_id])[0]
-            
-            song_data.append({
-                'album_name': album_name,
-                'track_name': track_name,
-                'artist_name': artist_name,
-                'genre': ', '.join(genres),
-                'danceability': audio_features['danceability'],
-                'energy': audio_features['energy'],
-                'speechiness': audio_features['speechiness'],
-                'acousticness': audio_features['acousticness'],
-                'valence': audio_features['valence'],
-                'instrumentalness': audio_features['instrumentalness']
-            })
+playlists = ['37i9dQZEVXbLiRSasKsNU9', '37i9dQZF1DXc3KygMa1OE7', '37i9dQZF1DWYtKpmml7moA', '37i9dQZF1DZ06evO1XahPz']
 
-        # Check if there are more pages of results
-        if results['next']:
-            results = sp.next(results)
-            tracks = results['items']
-        else:
-            results = None
+# Loop through each playlist
+for playlist_id in playlists:
+    results = sp.playlist_tracks(playlist_id)  # Using the playlist_tracks method
 
-    return pd.DataFrame(song_data)
+    # Extract IDs, Titles, and Artists
+    playlist_tracks_data = results['items']
+    playlist_tracks_id = [track['track']['id'] for track in playlist_tracks_data]
+    playlist_tracks_titles = [track['track']['name'] for track in playlist_tracks_data]
+    playlist_tracks_first_artists = [track['track']['artists'][0]['name'] for track in playlist_tracks_data]
 
-# put the data into a dataframe
-df = fetch_liked_songs_data()
-print(df.head())
-df.to_csv('spotify_data.csv', index=False)
+    # Extract Audio Features
+    features = sp.audio_features(playlist_tracks_id)
+
+    # Create a dataframe with audio features
+    features_df = pd.DataFrame(data=features, columns=features[0].keys())
+    features_df['album_name'] = ''  # Placeholder
+    features_df['track_name'] = playlist_tracks_titles
+    features_df['artist_name'] = playlist_tracks_first_artists
+
+    # Extract genre for the primary artist for each track
+    genres = []
+    for artist in playlist_tracks_first_artists:
+        artist_data = sp.search(q='artist:' + artist, type='artist')
+        artist_genre = artist_data['artists']['items'][0]['genres']
+        genres.append(', '.join(artist_genre))
+    features_df['genre'] = genres
+
+    # Rearrange columns and append to CSV
+    features_df = features_df[['album_name', 'track_name', 'artist_name', 'genre',
+                               'danceability', 'energy', 'speechiness', 
+                               'acousticness', 'valence', 'instrumentalness']]
+    features_df.to_csv('spotify_playlist_features.csv', mode='a', header=False, index=False)
---
+++ data-preprocessing/get_my_liked_songs.py
@@ -0,0 +1,63 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+# app credentials from spotify dashboard
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri="http://localhost:8086", scope="user-library-read"))
+
+# function to fetch liked songs from spotify
+def fetch_liked_songs_data():
+    """Fetch specified data for tracks in the user's Liked Songs."""
+    results = sp.current_user_saved_tracks()
+    tracks = results['items']
+    
+    song_data = []
+    while results:
+        for item in tracks:
+            track_obj = item['track']
+            track_name = track_obj['name']
+            track_id = track_obj['id']
+            album_name = track_obj['album']['name']
+            artist_name = track_obj['artists'][0]['name']
+            artist_id = track_obj['artists'][0]['id']  # Get the first artist's ID
+            
+            # Fetch artist to get the genre
+            artist = sp.artist(artist_id)
+            genres = artist['genres']
+            
+            # Fetch audio features
+            audio_features = sp.audio_features([track_id])[0]
+            
+            song_data.append({
+                'album_name': album_name,
+                'track_name': track_name,
+                'artist_name': artist_name,
+                'genre': ', '.join(genres),
+                'danceability': audio_features['danceability'],
+                'energy': audio_features['energy'],
+                'speechiness': audio_features['speechiness'],
+                'acousticness': audio_features['acousticness'],
+                'valence': audio_features['valence'],
+                'instrumentalness': audio_features['instrumentalness']
+            })
+
+        # Check if there are more pages of results
+        if results['next']:
+            results = sp.next(results)
+            tracks = results['items']
+        else:
+            results = None
+
+    return pd.DataFrame(song_data)
+
+# put the data into a dataframe
+df = fetch_liked_songs_data()
+print(df.head())
+df.to_csv('spotify_data.csv', index=False)
