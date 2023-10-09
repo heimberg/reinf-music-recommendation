cc01eba4f2bfa4569e7cb25407140932a0ebb60d
Matthias Heimberg <matthias.heimberg@students.ffhs.ch>
2023-10-09 16:46:29 +0200

    added environment structure

---
+++ RL-Model/envirnoment.py
@@ -0,0 +1,18 @@
+import gym
+from gym import spaces
+import pandas as pd
+import numpy as np
+
+
+class MusicRecommendationEnv(gym.Env):
+    def __init__(self, data_path, state_features):
+        return NotImplemented
+    
+    def step(self, action):
+        return NotImplemented
+    
+    def reset(self):
+        return NotImplemented
+    
+    def render(self, mode='human'):
+        return NotImplemented
