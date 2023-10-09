a0b5aebeff353894be90446b4ddbd0c6fd4ec5f2
Matthias Heimberg <matthias.heimberg@students.ffhs.ch>
2023-10-09 17:03:43 +0200

    constructor with Box Space implemented

--- RL-Model/envirnoment.py
+++ RL-Model/envirnoment.py
@@ -7,7 +7,7 @@ import numpy as np
 class MusicRecommendationEnv(gym.Env):
     # constructor
     # data_path: path to the dataset
-    # state_features: list of features to be used as state
+    # state_features: list of features to be used as state (danceability,energy,speechiness,acousticness,valence,instrumentalness,PCA_1,PCA_2)
     def __init__(self, data_path, state_features):
         super(MusicRecommendationEnv, self).__init__()
         
@@ -17,9 +17,18 @@ class MusicRecommendationEnv(gym.Env):
         self.current_state = None
         
         # define the action space
-        # only one action: choose the next song
-        self.action_space = spaces.Discrete(1)
-        return NotImplemented
+        # action: choose the next song, for every song in the dataset
+        self.action_space = spaces.Discrete(len(self.data))
+        
+        # set low and high limits for the observation space depending on the state features (0-1/-1-1)
+        low_limits = np.array(self.data[state_features].min())
+        high_limits = np.array(self.data[state_features].max())
+        
+        # define the observation space
+        # continuous state space -> Box Space (https://gymnasium.farama.org/api/spaces/fundamental/#gymnasium.spaces.Box)
+        # box space choosed because nuances in the state features could be possibly important
+        # alternative: Discrete Space (https://gymnasium.farama.org/api/spaces/fundamental/#gymnasium.spaces.Discrete)
+        self.observation_space = spaces.Box(low=low_limits, high=high_limits, dtype=np.float32)
     
     def step(self, action):
         return NotImplemented
